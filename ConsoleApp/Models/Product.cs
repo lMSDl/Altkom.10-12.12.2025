d8ac03ea5c46f7928b441e69f28e659a7a192de8
Paweł <pawel@msd.info.pl>
2025-12-10 12:32:16 +0100

    Budowa klasy

--- ConsoleApp/Models/Product.cs
+++ ConsoleApp/Models/Product.cs
@@ -1,8 +1,95 @@
-﻿namespace ConsoleApp.Models
+﻿//namespace - przestrzeń nazw, czyli adres pod którym "mieszka" klasa
+//namespace zaciągamy używając "using"
+namespace ConsoleApp.Models; // ; zamiast klemrek powoduje, że namespace jest zastosowany do całego pliku
+
+//klasa Product - klasa, która reprezentuje produkt
+//class - szablon opisujący zachowania i cechy obiektów (instancji klas), które są wytwarzane na jej podstawie
+//pełna nazwa klasy to <namespace>.<nazwa>
+//internal - modyfikator dostępu - oznacza, że z klasy można korzystać w obrębie projektu
+//public - modyfikator dostępu - oznacza, że z klasy można korzystać wszędzie
+//brak modyfikatora dostępu = internal (dla klas) - wybierany jest najniższy poziom dostępu
+/*internal*/ class Product
 {
-    internal class Product
+    //pole - zmienna, która przechowuje wartość
+    //private - oznacza dostęp tylko dla elementów danej klasy
+    //inne możliwe modyfikatory dostępu to: public, protected, internal, protected internal
+    //pola zazwyczaj są prywatne ze względu na hermetyzację, a dostęp realizowany jest przez metody dostępowe (getter i setter)
+    //nazwa pola zaczyna się od podkreślenia, żeby zaznaczyć, że jest to pole prywatne - konwencja C#
+    private DateTime _productionDate;
+
+    //budowa metody: <modyfikator dostępu> <typ zwracany lub void> <nazwa metody>()
+    //getter - do pobrania wartości pola - metoda zwraca typ zgodny z typem pola
+    public DateTime GetProductionDate()
     {
-        public string Name { get; set; }
-        public int Id { get; set; }
+        //instukcja zwracająca wynik działania metody - obowiązkowy gdy zadeklarowaliśmy, że klasa coś zwraca (jest inna niż void)
+        return _productionDate;
+    }
+
+    //setter - do ustawiania wartości - metoda przyjmuje parametr, który zostaje wpisany w odpowiednie pole (można dodać kod "obróbki danych")
+    //void - metoda nic nie zwaraca
+    public void SetProductionDate(DateTime productionDate)
+    {        
+        //metoda ustawiająca wartość pola - nie zwraca nic, więc jest typu void
+        _productionDate = productionDate.Date; //obróbka danych - zapisujemy tylko datę bez czasu
+
+        //odwołanie do bieżącej instancji klasy - this - używane gdy nazwa parametru jest taka sama jak nazwa pola
+        //this.productionDate = productionDate.Date; 
+    }
+
+
+    //Property - właściwość
+
+
+    //auto-property
+    //właściwość integruje w sobie pole i metody dostępowe (getter i setter)
+    public string Name { get;set; }
+
+    //jest możliwość zmiany modyfikatora dostępu dla getter lub setter (osobno)
+    public int Id { get; /*private*/ set; }
+
+
+    //full-property
+    //backfield do full-property - pozwala na dodatkowy kod w setterze i getterze
+    private DateTime _expirationDate;
+    public DateTime ExpirationDate
+    {
+        //getter dla property
+        get
+        {
+            return _expirationDate;
+        }
+        //setter dla property - posiada niejawny parametr o nazwie value
+        set
+        {
+            _expirationDate = value.Date; // kod obróbki danych - zapisujemy tylko datę bez czasu
+        }
+    }
+
+    //od .NET 10 można używać skróconej składni dla backfield w full-property używając słowa kluczowego field zamiast definiowania osobnego pola
+    public string Description
+    {
+        get
+        {
+            return field;
+        }
+
+        set
+        {
+            field = value;
+        }
+    }
+
+    //read-only property - właściwość tylko do odczytu
+    //nie posiada settera
+    public string FullInfo
+    {
+        get { return $"Id: {Id}, Name: {Name}, ProductionDate: {GetProductionDate()}, ExpirationDate: {ExpirationDate}"; }
+    }
+
+    //metoda realizucjąca to samo co powyższe read-only property
+    public string GetFullInfo()
+    {
+        return $"Id: {Id}, Name: {Name}, ProductionDate: {GetProductionDate()}, ExpirationDate: {ExpirationDate}";
     }
 }
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,40 +1,20 @@
 ﻿//instrukcje najwyższego poziomu(top - level statements) - są to instrukcje, które są wykonywane bezpośrednio w pliku, bez potrzeby definiowania klasy lub metody głównej.
 //wszystko co jest napisane w tym pliku jest traktowane jako kod, który będzie otoczony klasą Program i metodą Main podczas kompilacji
 
+
 using ConsoleApp.Models;
 
-Console.WriteLine("Hello, World!");
-
-//flaga Nullable w pliku csproj - pozwala na używanie typów wartościowych i referencyjnych w pododobny sposób w zakresie nullowania
-
-//typy wartościowe nie mogą być null
-//int a = null;
-
-int a = 5;
-//Nullable - opakowanie typów wartościowych, które pozwala na przypisanie wartości null do typów wartościowych
-Nullable<int> b = null; // Nullable type
-int? c = null; // Nullable type używająć shorthand syntax
-
+Console.WriteLine(typeof(Product).Name);
+Console.WriteLine(typeof(Product).Namespace);
+Console.WriteLine(typeof(Product).FullName);
 
 Product product = new Product();
-product.Name = "Sample Product";
-product.Id = 1;
+product.SetProductionDate(DateTime.Now);
+Console.WriteLine(product.GetProductionDate());
 
-Product? product2 = null;
+product.Name = "Laptop";
+Console.WriteLine(product.Name);
 
-Console.WriteLine($"Value of a: {a}");
-Console.WriteLine($"Value of b: {b}");
-Console.WriteLine($"Value of c: {c}");
+product.ExpirationDate = DateTime.Now.AddYears(1);
 
-Console.WriteLine($"Product Name: {product.Name}, Product Id: {product.Id}");
-if(product2 is not null)
-    Console.WriteLine($"Product Name: {product2.Name}, Product Id: {product2.Id}");
-
-
-ChangeProduct(product2);
-
-void ChangeProduct(Product product)
-{
-    product.Name = "Smartphone";
-    product.Id = 2;
-}
+Console.WriteLine(product.FullInfo);
