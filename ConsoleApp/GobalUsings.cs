656cafd699ccb646010fc1078ccaf4944aa88826
Paweł <pawel@msd.info.pl>
2025-12-10 11:20:03 +0100

    ImplicitUsings

---
+++ ConsoleApp/GobalUsings.cs
@@ -0,0 +1,3 @@
+﻿//global - pozwala na użycie przestrzeni nazw w całym projekcie bez konieczności dodawania dyrektywy using w każdym pliku źródłowym.
+//alternatywnie można globale kongigować w pliku projektu (.csproj) przy użyciu elementu <Using> wewnątrz <ItemGroup>.
+global using System;
