f6dbae69fc540516c2c2959e41e58ad095227b18
Paweł <pawel@msd.info.pl>
2025-12-10 11:11:52 +0100

    top-level statements

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,5 @@
-﻿namespace ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//instrukcje najwyższego poziomu(top - level statements) - są to instrukcje, które są wykonywane bezpośrednio w pliku, bez potrzeby definiowania klasy lub metody głównej.
+//wszystko co jest napisane w tym pliku jest traktowane jako kod, który będzie otoczony klasą Program i metodą Main podczas kompilacji
+
+Console.WriteLine("Hello, World!");
+
